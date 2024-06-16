b710a5249e78bd0a33cdec8c30dc6d7b3c9cec33
name <asdfad@asdfasd.com>
2024-06-16 14:35:12 -0700

    Initial Commit

---
+++ Swift-Binding-State/Swift_Binding_StateApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_Binding_StateApp.swift
+//  Swift-Binding-State
+//
+//  Created by aaa on 2024-06-16.
+//
+
+import SwiftUI
+
+@main
+struct Swift_Binding_StateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Swift-Binding-StateUITests/Swift_Binding_StateUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Swift_Binding_StateUITestsLaunchTests.swift
+//  Swift-Binding-StateUITests
+//
+//  Created by aaa on 2024-06-16.
+//
+
+import XCTest
+
+final class Swift_Binding_StateUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
