ec60f1847bf923d93ac8bead4abd4cc7a0d9c79d
name <asdfad@asdfasd.com>
2024-06-16 14:44:13 -0700

    first

---
+++ README.md
@@ -0,0 +1,8 @@
+# Swift, Form and Section 
+
+```
+
+```
+
+<img src="swift_button.png" alt="drawing" width="400"/>
+
--- Swift-Binding-State/ContentView.swift
+++ Swift-Binding-State/ContentView.swift
@@ -7,15 +7,49 @@
 
 import SwiftUI
 
+struct Fruit : Identifiable{
+    var id = UUID()
+    var color : Color
+    var name : String
+}
+
 struct ContentView: View {
+    @State private var fruit : [Fruit] = [ Fruit(color:Color.green, name: "Apple"),
+                                         Fruit(color:Color.cyan, name:"Orange")]
+    @State private var selectedName: String = ""
+    @State private var selectedFruit: Fruit = Fruit(color : Color.red, name:"init fruit")
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List(fruit) { item in
+                Text(item.name)
+                    .onTapGesture {
+                        selectedFruit = item
+                    }
+            }
+            .frame(height: 200)
+            
+            ChildView(selectedFruit: $selectedFruit)
+                .padding()
         }
-        .padding()
+    }
+}
+
+struct ChildView: View {
+    @Binding var selectedFruit: Fruit
+    
+    var body: some View {
+        TextField("Edit item", text: $selectedFruit.name)
+            .padding()
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .foregroundColor(selectedFruit.color)
+            .background(RoundedRectangle(cornerRadius: 10).fill(Color.blue))
+        Button("Print in console"){
+            print("item => \(selectedFruit.name)")
+            
+        }
+        .buttonStyle(.borderedProminent)
+
     }
 }
 
